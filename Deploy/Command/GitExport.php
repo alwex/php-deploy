eaec0923745317d677f3f476e2d738c9d12a9886
Alexandre GUIDET <aguidet@hi-media.com>
2015-02-11 11:46:25 +0100

    ajout configuration pour le bin

---
+++ Deploy/Command/GitExport.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: aguidet
+ * Date: 11/02/15
+ * Time: 11:43
+ */
+
+namespace Deploy\Command;
+
+
+class GitExport extends AbstractCommand {
+
+    /**
+     * execute command and php tasks
+     * return the execution status as an integer
+     *
+     * @return int
+     */
+    public function run()
+    {
+    }
+}
